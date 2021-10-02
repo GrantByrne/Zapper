3c70a872d3a9cbe3a73888bf6db67b792c9a0008
Grant Byrne <grant@grantbyrne.com>
2021-10-01 20:55:42 -0400

    Initial commit of laying down the framework.

---
+++ Zapper.Console/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using HashtagChris.DotNetBlueZ;
+using HashtagChris.DotNetBlueZ.Extensions;
+
+namespace Zapper.Console
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+
+            var adapter = (await BlueZManager.GetAdaptersAsync()).FirstOrDefault();
+
+            var devices = await adapter.GetDevicesAsync();
+
+            foreach (var d in devices)
+            {
+                var name = "";
+
+                try
+                {
+                    name = await d.GetNameAsync();
+                }
+                catch (Exception)
+                {
+                }
+                
+                System.Console.WriteLine($"Found device! {name}");
+            }
+
+            System.Console.ReadKey();
+        }
+    }
+}
---
+++ Zapper.Core/BluetoothManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HashtagChris.DotNetBlueZ;
+using HashtagChris.DotNetBlueZ.Extensions;
+
+namespace Zapper.Core
+{
+    public class BluetoothManager : IDisposable
+    {
+        private Adapter _adapter;
+        
+        public async Task StartUp()
+        {
+            var adapters = await BlueZManager.GetAdaptersAsync();
+            _adapter = adapters.FirstOrDefault();
+        }
+
+        public async Task<IEnumerable<string>> GetBluetoothDevices()
+        {
+            var devices = await _adapter.GetDevicesAsync();
+
+            var names = new List<string>();
+            
+            foreach (var d in devices)
+            {
+                string name = null;
+
+                try
+                {
+                    name = await d.GetNameAsync();
+                }
+                catch (Exception ex)
+                {
+                    
+                }
+
+                if (!string.IsNullOrWhiteSpace(name))
+                {
+                    names.Add(name);
+                }
+            }
+
+            return names;
+        }
+
+        public void Dispose()
+        {
+            _adapter?.Dispose();
+        }
+    }
+}
