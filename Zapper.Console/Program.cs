4cc80e69bde3a3f75c34979f0d39ee1e16b58289
Grant Byrne <grant@grantbyrne.com>
2021-10-28 00:48:21 -0400

    Updated the console project to serve as a test exe for the on key press events.

--- Zapper.Console/Program.cs
+++ Zapper.Console/Program.cs
@@ -1,9 +1,15 @@
-﻿namespace Zapper.Console
+﻿using Zapper.Core.KeyboardMouse;
+
+namespace Zapper.Console
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            using var aggHandler = new AggregateInputReader();
+
+            aggHandler.OnKeyPress += (e) => { System.Console.WriteLine($"Code:{e.Code} State:{e.State}"); };
+            
             System.Console.ReadLine();
         }
     }
