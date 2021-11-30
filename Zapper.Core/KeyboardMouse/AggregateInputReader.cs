f43277011711042ae95ccdeb19bb91e5078fea6f
Grant Byrne <grant@grantbyrne.com>
2021-11-29 23:07:29 -0500

    #13 - Added in some code that will automatically pick up devices that are plugged in. Also added in some code that will remove devices that have been removed.

--- Zapper.Core/KeyboardMouse/AggregateInputReader.cs
+++ Zapper.Core/KeyboardMouse/AggregateInputReader.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Timers;
 using Microsoft.Extensions.Logging;
 using Zapper.Core.KeyboardMouse.Abstract;
 
@@ -8,22 +10,21 @@ namespace Zapper.Core.KeyboardMouse
 {
     public class AggregateInputReader : IDisposable, IAggregateInputReader
     {
-        private List<InputReader> _readers = new();
+        private readonly ILogger<InputReader> _inputReaderLogger;
+        
+        private Dictionary<string, InputReader> _readers = new();
         
         public event InputReader.RaiseKeyPress OnKeyPress;
 
         public AggregateInputReader(ILogger<InputReader> inputReaderLogger)
         {
-            var files = Directory.GetFiles("/dev/input/", "event*");
+            _inputReaderLogger = inputReaderLogger;
 
-            foreach (var file in files)
-            {
-                var reader = new InputReader(file, inputReaderLogger);
-                
-                reader.OnKeyPress += ReaderOnOnKeyPress;
-
-                _readers.Add(reader);
-            }
+            var timer = new Timer();
+            timer.Interval = 10 * 1000;
+            timer.Enabled = true;
+            timer.Elapsed += (_, _) => Scan(); 
+            timer.Start();
         }
 
         private void ReaderOnOnKeyPress(KeyPressEvent e)
@@ -31,9 +32,37 @@ namespace Zapper.Core.KeyboardMouse
             OnKeyPress?.Invoke(e);
         }
 
+        private void Scan()
+        {
+            var files = Directory.GetFiles("/dev/input/", "event*");
+
+            foreach (var file in files)
+            {
+                if (_readers.ContainsKey(file))
+                {
+                    continue;
+                }
+                
+                var reader = new InputReader(file, _inputReaderLogger);
+                
+                reader.OnKeyPress += ReaderOnOnKeyPress;
+
+                _readers.Add(file, reader);
+            }
+
+            var deadReaders = _readers.Values.Where(r => r.Faulted);
+
+            foreach (var dr in deadReaders)
+            {
+                _readers.Remove(dr.Path);
+                dr.OnKeyPress -= ReaderOnOnKeyPress;
+                dr.Dispose();
+            }
+        }
+
         public void Dispose()
         {
-            foreach (var d in _readers)
+            foreach (var d in _readers.Values)
             {
                 d.OnKeyPress -= ReaderOnOnKeyPress;
                 d.Dispose();
