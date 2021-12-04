8a3b5ad0381047a56ee7620a29863f9926d02cae
Grant Byrne <grant@grantbyrne.com>
2021-12-04 15:37:40 -0500

    #34 - Added support for logging to a file.

--- Zapper.Web/Program.cs
+++ Zapper.Web/Program.cs
@@ -1,14 +1,37 @@
-using System;
+    using System;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+    using Serilog;
+    using Serilog.Events;
 
-namespace Zapper.Web
+    namespace Zapper.Web
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.File("zapper.log")
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting web host");
+                CreateHostBuilder(args).Build().Run();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
