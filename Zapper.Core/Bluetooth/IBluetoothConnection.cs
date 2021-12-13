5fa32cf19b567b84254d78890bf6e8065b8351da
Grant Byrne <grant@grantbyrne.com>
2021-12-12 19:23:03 -0500

    Updated the BluetoothConnection interface to support connecting to bluetooth devices.

--- Zapper.Core/Bluetooth/IBluetoothConnection.cs
+++ Zapper.Core/Bluetooth/IBluetoothConnection.cs
@@ -1,11 +1,17 @@
 using System.Threading.Tasks;
+using HashtagChris.DotNetBlueZ;
 
 namespace Zapper.Core.Bluetooth;
 
 public interface IBluetoothConnection
 {
     Task Start();
+    
     Task ScanForDevices();
+    
     void Stop();
+    
     event BluetoothConnection.RaiseBluetoothDeviceFound OnBluetoothDeviceFound;
+    
+    Task Connect(Device device);
 }
